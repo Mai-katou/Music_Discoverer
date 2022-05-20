2f66322780ae6d156597d18b1081e3679daffc28
Mai-katou <yoruichi.shunkoh@gmail.com>
2022-05-20 12:20:37 +0000

    [Update]devise設定(user)側

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
   root to: "homes#top"
+  get "homes/about" => "homes#about", as: 'about_path'
+
+  devise_for :admin,controllers:{
+    sessions:"admin/sessions"
+  }
+  devise_for :users, controllers:{
+    registrations:"public/registrations",
+    sessions:'public/sessions'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
