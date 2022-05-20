36fd5a9842cad6b81c1dc4e73c3a4a8bb0fd1910
Mai-katou <yoruichi.shunkoh@gmail.com>
2022-05-20 11:11:17 +0000

    [Update]ActiveStorage導入/Devise導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
   config.hosts << "a2c0670d617340768b1458832ab63524.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.active_job.queue_adapter = :inline
 end
